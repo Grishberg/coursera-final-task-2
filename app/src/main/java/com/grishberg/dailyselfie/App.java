313bc77d672a56686e14fada0a0a39ab9a6ef9a6
grishberg <grishberg@gmail.com>
2016-04-30 18:50:27 +0700

    added strict mode

--- app/src/main/java/com/grishberg/dailyselfie/App.java
+++ app/src/main/java/com/grishberg/dailyselfie/App.java
@@ -2,7 +2,10 @@ package com.grishberg.dailyselfie;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.StrictMode;
 import android.util.Log;
+
+import com.nostra13.universalimageloader.BuildConfig;
 
 /**
  * Created by grishberg on 28.04.16.
@@ -14,6 +17,20 @@ public class App extends Application {
     @Override
     public void onCreate() {
         Log.d(TAG, "onCreate: DailySelfie");
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()   // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
         super.onCreate();
         sAppContext = getApplicationContext();
     }
